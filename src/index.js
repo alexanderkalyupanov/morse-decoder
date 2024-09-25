788eded671df3a5159aa43a203f6582a48d73ae1
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2024-09-25 16:42:58 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const morseArray = expr.match(/.{1,10}/g);
+    const dec = morseArray.map(item => {
+        if (item == "**********") {
+            return " ";
+        }
+
+        let morseSymbol = "";
+        for (let i = 0; i < item.length; i+=2) {
+            let pair = item.slice(i, i+2)
+            if (pair === "10") {
+                morseSymbol += "."
+            }
+            else if (pair === "11") {
+                morseSymbol += "-"
+            }
+        }
+        console.log(morseSymbol)
+
+            return MORSE_TABLE[morseSymbol]
+    })
+        return dec.join("");
+} 
+
 
 module.exports = {
     decode
